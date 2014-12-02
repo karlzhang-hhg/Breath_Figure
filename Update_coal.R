156209805ec797ac52a671f728d3187e2a0e7bd2
Kungang <kz29@duke.edu>
2014-12-02 07:47:13 -0500

    Find a bug for bfobj[[as.character(big)]]&deat.t<<-t+dtp;
    initially, I used '<-' so that change cannot be made in global environment

--- Update_coal.R
+++ Update_coal.R
@@ -9,6 +9,7 @@ Update_coal<-function(ncoal,t,small,big,edge.circ,ds,h,w)
 #   stopifnot(is.integer(small))
 #   stopifnot(is.integer(big))
   #convert the index number to character
+  print(c("ncoal:",ncoal))
   small=as.character(small)
   big=as.character(big)
   #update information for the small droplet after a coalescence
@@ -19,7 +20,9 @@ Update_coal<-function(ncoal,t,small,big,edge.circ,ds,h,w)
   coalall[[ncoal]]<<-list(ind=ncoal,t=t,invo.drop=matrix(as.integer(c(small,big)),nrow=1),delt.s=ds)#invo.drop initially is NULL
   names(coalall)[ncoal]<<-as.character(ncoal)
   ######################################################################
-  bfobj[[small]]$coal[[length(bfobj[[small]]$coal)+1L]]<<-coalall[[ncoal]]
+  
+  
+  
   #update information for the big droplet after a coalescence
   bfobj[[big]]$r<<-((bfobj[[small]]$r)^3+(bfobj[[big]]$r)^3)^(1/3)
   #Consider the circlic effect
@@ -41,12 +44,27 @@ Update_coal<-function(ncoal,t,small,big,edge.circ,ds,h,w)
   }
   #The information of coalescence from the small absorbed droplet should be included into the big one
   bfobj[[big]]$log.tr<<-rbind(bfobj[[small]]$log.tr,bfobj[[big]]$log.tr,c(t,bfobj[[big]]$posi[1],bfobj[[big]]$posi[2],bfobj[[big]]$r))
-  bfobj[[big]]$abso.drop<<-c(bfobj[[small]]$abso.drop,bfobj[[big]]$abso.drop,as.integer(small))
-  if (!is.null(bfobj[[small]]$coal))
+  bfobj[[big]]$abso.drop<<-c(bfobj[[big]]$abso.drop,bfobj[[small]]$abso.drop,as.integer(small))
+  
+  #Update coalescence information
+  if (length(bfobj[[small]]$coal)!=0) #even list() is not NULL. it just has 0 length
   {
-    bfobj[[big]]$coal=c(bfobj[[small]]$coal,bfobj[[big]]$coal)
+    bfobj[[big]]$coal<<-as.list(c(bfobj[[big]]$coal,bfobj[[small]]$coal))
   }
+#   if (is.null(bfobj[[big]]$coal[[1]]))
+#   {
+#     bfobj[[big]]$coal[[length(bfobj[[big]]$coal)+1L]]<<-coalall[[ncoal]]
+#   }
+#   else
+#   {
   bfobj[[big]]$coal[[length(bfobj[[big]]$coal)+1L]]<<-coalall[[ncoal]]
+#   }
+  bfobj[[small]]$coal[[length(bfobj[[small]]$coal)+1L]]<<-coalall[[ncoal]]
+  if (is.null(bfobj[[big]]$coal[[1]]))
+  {
+    print(c(bfobj[[big]],bfobj[[small]]))
+    stopifnot(!is.null(bfobj[[big]]$coal[[1]]))
+  }
   #   bfallobj[names(bfobj)]<<-bfobj
   #   bfobj<<-bfobj[!(names(bfobj) %in% as.character(delete.list))]
 }
