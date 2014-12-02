0df91a1da3265300f1331abff9f04caed0b126b1
Kungang <kz29@duke.edu>
2014-12-02 07:46:02 -0500

    save data to file

--- Run.R
+++ Run.R
@@ -4,14 +4,17 @@ source("Evol.R")
 
 h=1
 w=1
-nstep=200L
+nstep=1000L
 lambda=20
-alpha=0.08
+alpha=0.1
 rcr=0.03
-dr=1.05
+dr=0.001
 dt=0.01
 nuclr=0.0001
 
+phypar=list(height=h,width=w,num_step=nstep,nucleation_density=lambda,
+            condensing_rate=alpha,critical_radius=rcr,repulsive_length=dr,
+            time_step=dt,nucleate_radius=nuclr)
 
 t=0
 tseq=t
@@ -25,3 +28,15 @@ coalall=NULL
 nucl.num=0
 
 Evol(h,w,nstep,lambda,alpha,rcr,dr,dt,nuclr)
+
+old.wd=getwd()
+setwd(paste0(old.wd,"/Dat/Run1"))
+save(phypar,file="phypar")
+save(bfallobj,file="bfallobj")
+save(bfobj,file="bfobj")
+save(tseq,file="tseq")
+save(depart.list,file="depart.list")
+save(coalall,file="coalall")
+save(coalseq,file="coalseq")
+save(nucl.num,file="nucl.num")
+setwd(old.wd)
