cda9d89a6233ed06b9d1945d8c719b79d2aa5e64
Kungang <kz29@duke.edu>
2014-12-04 19:38:50 -0500

    add upper_ratio for jumping

--- Run.R
+++ Run.R
@@ -4,17 +4,21 @@ source("Evol.R")
 
 h=1
 w=1
-nstep=1000L
-lambda=20
-alpha=0.1
+nstep=2000L
+lambda=30
+alpha=0.15
 rcr=0.03
 dr=0.001
 dt=0.01
 nuclr=0.0001
+#the upper ratio for jumping happen
+#in order for jumping happening, the merged drop size should smaller
+#than up_ratio times of smaller droplet
+up_ratio=3
 
 phypar=list(height=h,width=w,num_step=nstep,nucleation_density=lambda,
             condensing_rate=alpha,critical_radius=rcr,repulsive_length=dr,
-            time_step=dt,nucleate_radius=nuclr)
+            time_step=dt,nucleate_radius=nuclr,upper_ratio=up_ratio)
 
 t=0
 tseq=t
@@ -30,7 +34,7 @@ nucl.num=0
 Evol(h,w,nstep,lambda,alpha,rcr,dr,dt,nuclr)
 
 old.wd=getwd()
-setwd(paste0(old.wd,"/Dat/Run1"))
+setwd(paste0(old.wd,"/Dat/Run6"))
 save(phypar,file="phypar")
 save(bfallobj,file="bfallobj")
 save(bfobj,file="bfobj")
