a47a8f0e5a3022478dd4114a473ee25a0878d83f
Kungang <kz29@duke.edu>
2014-12-11 22:20:20 -0500

    Statistical analysis

--- Analysis.R
+++ Analysis.R
@@ -37,7 +37,7 @@ for (i in 1:length(bfallobj))
     }
     if (k>nrow(bfallobj[[i]]$log.tr)) break
   }
-  print(i)
+  #print(i)
 }
 
 #Find maximum r for range of density plot
@@ -98,6 +98,33 @@ for (i in 1:length(plotc))
   dev.off()
 }
 
+plot_drop_distr_t<-function(plotc,size_dis_t,tseq,y_lim,rmax)
+{
+  lwd<-seq(0.5,2.5,length=length(plotc))
+  plot(density(size_dis_t[[plotc[1]+1]][,2],from=0),xlim=c(0,rmax),ylim=y_lim,
+       main=paste0("Drop size distribution at Different time"),
+       xlab="Radius",ylab="Density",lty=1,lwd=lwd[1])
+  legend_name<-c(paste0("t=",tseq[plotc[1]+1]-((tseq[plotc[1]+1]*1000)%%1)/1000))
+  if (length(plotc)==1) return()
+  for (i in 2:length(plotc))
+  {
+    legend_name<-c(legend_name,paste0("t=",tseq[plotc[i]+1]-((tseq[plotc[i]+1]*1000)%%1)/1000))
+    lines(density(size_dis_t[[plotc[i]+1]][,2],from=0),col=i,lty=i,lwd=lwd[i])
+  }
+  legend(x = "topright",legend = legend_name,
+         bty="n",
+         text.col=1:length(plotc),
+         lty=1:length(plotc),pch=NA,
+         lwd=lwd,
+         col=1:length(plotc))
+  par("yaxs"="r")
+}
+
+plotc<-c(10,50,100,300,400,seq(500,4000,500))
+
+pdf("Asymptotic Distribution.pdf")
+plot_drop_distr_t(plotc,size_dis_t,tseq,c(0,80),rmax)
+dev.off()
 
 #Jumping size, time, and number of coalescence
 jump_info=NULL
@@ -143,7 +170,7 @@ for(i in 1:length(coalall))
 }
 
 pdf("coal_times-t.pdf")
-plot(tseq,accu_coal_times_t,pch=1,cex=0.1,main="Times of coalecence before t",xlab="t",ylab="Accumulated times of coalescence")
+plot(tseq,accu_coal_times_t,pch=1,cex=0.1,main="Times of coalescence before t",xlab="t",ylab="Accumulated times of coalescence")
 dev.off()
 
 #Sort information in bfallobj
@@ -167,24 +194,176 @@ for(i in 1:length(bfallobj_sort))
   }
 }
 
+# #Coalsecence times v.s. size
+# coal_drop_t=matrix(rep(NA,length(bfallobj_sort)*length(tseq)),ncol=length(tseq))
+# for(i in 1:length(bfallobj_all))
+# {
+#   k=1
+#   for(j in 1:length(tseq))
+#   {
+#     if(bfallobj[[i]]$coal[[k]]$t==tseq[j])
+#     {
+#       coal_drop_t[i,j]=bfallobj[[i]]$log.tr[]
+#       k=k+1
+#       if (k>nrow(bfall))
+#     }
+#   }
+# }
+
 #Coalsecence times v.s. size
-coal_drop_t=matrix(rep(NA,length(bfallobj)*length(tseq)),ncol=length(tseq))
-for(i in 1:length(bfallobj))
+coal_drop<-function(bfallobj_t,tseq)
 {
-  k=1
-  for(j in 1:length(tseq))
+  coal_drop_t=list()
+  for(i in 1:length(bfallobj_t))
   {
-    if(bfallobj[[i]]$coal[[k]]$t==tseq[j])
+    k=1
+    for(j in 1:length(tseq))
     {
-      coal_drop_t[i,j]=bfallobj[[i]]$log.tr[]
-      k=k+1
-      if (k>nrow(bfall))
+      if (length(bfallobj_t[[i]]$coal)==0) break
+      if(bfallobj_t[[i]]$coal[[k]]$t==tseq[j])
+      {
+        r<-max(bfallobj_t[[i]]$log.tr[bfallobj_t[[i]]$log.tr[,1]==bfallobj_t[[i]]$coal[[k]]$t,4])
+        if (k>length(coal_drop_t))
+        {
+          coal_drop_t<-c(coal_drop_t, list(matrix(c(bfallobj_t[[i]]$coal[[k]]$t,r),nrow=1)))
+        }
+        else
+        {
+          coal_drop_t[[k]]<-rbind(coal_drop_t[[k]],c(bfallobj_t[[i]]$coal[[k]]$t,r))
+        }
+        k=k+1
+        if (k>length(bfallobj_t[[i]]$coal)) break
+      }
     }
+  }
+  return(coal_drop_t)
+}
+
+coal_drop_t<-coal_drop(bfallobj_sort,tseq)
+
+
+#Find the maximum time of coalescence and return the index of the droplets in bfallobj_sort or bfallobj
+maxncoal=0
+maxncoal_ind=0
+
+for (i in 1:length(bfallobj_sort))
+{
+  if (length(bfallobj_sort[[i]]$coal)>maxncoal)
+  {
+    maxncoal=length(bfallobj_sort[[i]]$coal)
+    maxncoal_ind=bfallobj_sort[[i]]$ind
   }
 }
 
+maxncoal_drop<-bfallobj_sort[[maxncoal_ind]]
+coal_t<-unlist(lapply(maxncoal_drop$coal,function(x) x$t))
+
+
+plot(1:length(maxncoal_drop$coal),maxncoal_drop$log.tr[maxncoal_drop$log.tr[,1] %in% coal_t,4],
+     pch=1,cex=0.3,
+     main="Times of coalescence v.s. size",xlab="# of coalescence",
+     ylab="Size of droplets with /n the most coalescence times")
+
+
+CEX=0.7
+
+pdf("coal_times-size.pdf")
+plot(1:length(coal_drop_t),unlist(lapply(coal_drop_t,function(x) mean(x[,2]))),
+     pch=1,cex=CEX,main="Times of coalescence v.s. size",xlab="# of coalescence",ylab="Size of droplets")
+points(1:length(coal_drop_t),unlist(lapply(coal_drop_t,function(x) mean(x[,2]))) +sqrt(unlist(lapply(coal_drop_t,function(x) var(x[,2])))),
+       pch=1,cex=CEX,col="red")
+points(1:length(coal_drop_t),unlist(lapply(coal_drop_t,function(x) mean(x[,2]))) -sqrt(unlist(lapply(coal_drop_t,function(x) var(x[,2])))),
+       pch=1,cex=CEX,col="red")
+points(1:length(maxncoal_drop$coal),maxncoal_drop$log.tr[maxncoal_drop$log.tr[,1] %in% coal_t,4],
+       pch=3,cex=CEX,col="blue")
+legend(x = "bottomright",legend = c("Mean radius","1 STD upper","1 STD lower", "The case with largest coalescence time"),
+       bty="n",
+       text.col=c("black","red","red","blue"),
+       lty=NA,pch=c(1,1,1,3),
+       lwd=1,
+       col=c("black","red","red","blue"))
+par("yaxs"="r")
+dev.off()
 
 #At every moment, the size distribution of existing droplets with different times of coalescence
+
+drop_gener_t<-function(bfallobj_t,tseq,t,y_lim,nsample)
+{
+  for (i in 1:length(tseq))
+  {
+    if (t<tseq[i])
+    {
+      nt<-i-1
+      break
+    }
+  }
+  drop_list<-NULL
+  for (i in 1:length(bfallobj_t))
+  {
+    if (bfallobj_t[[i]]$deat.t>=t | bfallobj_t[[i]]$jump.t>=t)
+    {
+      drop_list<-c(drop_list,i)
+    }
+  }
+  
+  plot_drop_gener_t<-function(drop_gener_t,t)
+  {
+    plot(density(drop_gener_t1[[1]][,2],from=0,to=2*rmax),xlim=c(0,rmax),ylim=y_lim,
+         main=paste("Distribution of different \n generation (coalescence) at t=",t),
+         xlab="Drop size",
+         ylab="Density")
+    legend_name<-c(paste0("g1","-",length(drop_gener_t[[1]])))
+    plotted_one<-c(1)
+    for (i in 2:length(drop_gener_t))
+    {
+      if (length(drop_gener_t[[i]])>nsample)
+      {
+        lines(density(drop_gener_t1[[i]][,2]),col=i)
+        legend_name<-c(legend_name,paste0("g",i,"-",length(drop_gener_t[[i]])))
+        plotted_one<-c(plotted_one,i)
+      }
+    }
+    legend(x = "topright",legend = legend_name,
+           bty="n",
+           text.col=1:length(drop_gener_t)[plotted_one],
+           lty=1,pch=NA,
+           lwd=1,
+           col=1:length(drop_gener_t)[plotted_one])
+    par("yaxs"="r")
+  }
+  
+  drop_gener_t1<-coal_drop(bfallobj_t[drop_list],tseq[1:nt])
+  plot_drop_gener_t(drop_gener_t1,t)
+  
+  return(drop_gener_t1)
+}
+
+for (i in 1:23)
+{
+  pdf(paste0("Distr-gener-",i/10,".pdf"))
+  drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,i/10,c(0,100),10)
+  dev.off()
+}
+
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,0.5,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,0.8,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,0.9,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.0,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.1,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.2,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.3,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.4,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.5,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.6,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.7,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.8,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,1.9,c(0,100),10)
+drop_gener_t1<-drop_gener_t(bfallobj_sort,tseq,2.0,c(0,100),10)
+
+
+
+
+
 
 
 
